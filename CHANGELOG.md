ece0417b4282bf161d5d21cb968e70fdb1268e35
Duane Johnson <duane@mainframe.com>
2018-08-04 21:28:50 -0600

    Release 0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,15 @@
 - Initial release
 - Copied from https://github.com/tbh1/sublime-notes
 - Adapted for Visual Studio Code
+
+## [0.2]
+- Changed heading color to use markup.heading.notes
+- Add new "date" heading syntax with highlight, e.g. `[2018-08-04]`
+- Added taskCycleForward / taskCycleBackward to check tasks
+  as incomplete, complete, or canceled:
+
+  [ ] task
+  [√] done task
+  [x] canceled task
+
+  By default, bound to Option+L and Option+H respectively.
---
+++ vscode/extension.js
@@ -0,0 +1,74 @@
+const vscode = require('vscode')
+const { Range, Position, commands, workspace, window } = vscode
+
+exports.activate = async function activate(context) {
+    context.subscriptions.push
+        ( commands.registerTextEditorCommand
+            ( 'notes.cycleTaskForward', cycleTaskForward )
+        , commands.registerTextEditorCommand
+            ( 'notes.cycleTaskBackward', cycleTaskBackward )
+        )
+
+    function swap(obj){
+        let ret = {};
+        for(let key in obj){
+            ret[obj[key]] = key;
+        }
+        return ret;
+    }
+
+    const nextStateLookup = {
+        "[ ]": "[√]",
+        "[√]": "[x]",
+        "[x]": "[ ]"
+    }
+
+    function nextTaskState(currentState) {
+        const lookup = nextStateLookup[currentState]
+        if (lookup) {
+            return lookup
+        } else {
+            return currentState
+        }
+    }
+
+    function prevTaskState(currentState) {
+        const lookup = swap(nextStateLookup)[currentState]
+        if (lookup) {
+            return lookup
+        } else {
+            return currentState
+        }
+    }
+
+    function cycleTaskForward(editor) {
+        cycleTask(editor, nextTaskState)
+    }
+
+    function cycleTaskBackward(editor) {
+        cycleTask(editor, prevTaskState)
+    }
+
+    function cycleTask(editor, nextStateFn) {
+        editor.edit(editBuilder => {
+            editor.selections.forEach(selection => {
+                let lineNo = selection.start.line
+                while (lineNo <= selection.end.line) {
+                    const line = editor.document.lineAt(lineNo)
+                    const m = line.text.match(/^\s*(\[.?\])/ )
+                    if (m) {
+                        const braceMatch = m[1];
+                        const position = line.text.indexOf(braceMatch)
+                        const range = new Range(
+                            new Position(lineNo, position),
+                            new Position(lineNo, position + 3)
+                        )
+                        const newText = nextStateFn(braceMatch)
+                        editBuilder.replace(range, newText)
+                    }
+                    lineNo++
+                }
+            })
+        })
+    }
+}
